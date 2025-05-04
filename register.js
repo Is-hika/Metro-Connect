4a808075f8c81b65626a7a9c57f6a727f15ad718
ishika kumari <167968196+Is-hika@users.noreply.github.com>
2025-05-04 22:20:22 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCmox2RGGt7a5qznQuhTnDE0hWTqC_pnIg",
+    authDomain: "metro-connect-45207.firebaseapp.com",
+    projectId: "metro-connect-45207",
+    storageBucket: "metro-connect-45207.firebasestorage.app",
+    messagingSenderId: "152834448719",
+    appId: "1:152834448719:web:473adde75c02df0f82793a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
+
